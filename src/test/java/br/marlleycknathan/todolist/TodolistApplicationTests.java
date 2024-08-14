6a35c03a5c7f3f4c880fe741c603564619f1b138
marlleyck <marlleyck@hotmail.com>
2024-08-14 14:53:38 -0300

    feat: Initial configuration

---
+++ src/test/java/br/marlleycknathan/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.marlleycknathan.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
