b10a74c89bf6704e0cb08b29604c4a9e00c3a858
marlleyck <marlleyck@hotmail.com>
2024-08-14 15:27:07 -0300

    feat: create Todo service

---
+++ src/main/java/br/marlleycknathan/todolist/service/TodoService.java
@@ -0,0 +1,39 @@
+package br.marlleycknathan.todolist.service;
+
+import br.marlleycknathan.todolist.entity.Todo;
+import br.marlleycknathan.todolist.repository.TodoRepository;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoService {
+    private final TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    public List<Todo> create(Todo todo) {
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> list() {
+        Sort sort = Sort.by("priority").descending().and(
+                Sort.by("name").ascending()
+        );
+        return todoRepository.findAll(sort);
+    }
+
+    public List<Todo> update(Todo todo) {
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> delete(Long id) {
+        todoRepository.deleteById(id);
+        return list();
+    }
+}
